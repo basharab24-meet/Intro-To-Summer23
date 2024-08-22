ca4f93cf1ab9ec4cbe24b651d2c537a6e3a1f684
basharab24-meet <bashargeo@gmail.com>
2024-08-22 14:32:43 +0300

    divorce

--- Fortune-teller/app.py
+++ Fortune-teller/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,redirect,request
 import random
 
 app = Flask(__name__,
@@ -6,15 +6,28 @@ template_folder='templetes',
 static_folder='static')
 
 
-@app.route('/home')
+@app.route('/home', methods=["POST","GET"])
 def home():
-    return render_template("home.html")
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        birthday = request.form['birthday']
+        print(birthday)
+        return redirect(url_for('fortune',
+            b = birthday))
 
-@app.route('/fortune')
-def fortune():
-    future=["millions","marrage","no job","kids","divorse","9-5 job","elite athlete","MEET instructor","a trip around the world","free PC"]
-    f= random.choice(future)
-    return render_template("fortune.html", f=f)
+
+
+@app.route('/fortune/<b>',methods=["POST","GET"])
+def fortune(b):
+    future=["millions","marrige","no job","kids","divorse","9-5 job","elite athlete","MEET instructor","a trip around the world","free PC"]
+    f= random.choice(future) 
+    len_b=len(b)
+    if len_b > 10:
+        return render_template("home.html")
+    print(len_b)
+    fortune_final=future[len_b]
+    return render_template("fortune.html", f=fortune_final)
 
 
 
